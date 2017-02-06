949b6aa3d65f8a5732689d3714d84d31c733beab
TomasR <Rakusan2@gmail.com>
2017-02-06 18:23:57 -0500

    Add uglify to gulp js build chain

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require("browserify");
 var tsify = require("tsify");
 var source = require("vinyl-source-stream");
 var watchify = require("watchify");
+var uglify = require("gulp-uglify");
 var sassOptions = {
     outputStyle: 'expanded'
 }, crxOptions = function () {
@@ -15,34 +16,39 @@ var sassOptions = {
         filename: 'eqdExtra.crx'
     };
 };
-function combinejs() {
+function combinejs(update, minify) {
     [['lib/backScript.ts', 'backScript.js'], ['lib/eqd-inject.ts', 'eqd-inject.js'], ['lib/eqdComment-inject.ts', 'eqdComment-inject.js']].forEach(function (f) {
         var b = watchify(browserify({ entries: f[0] })), bundle = function () {
-            b.bundle()
-                .pipe(source(f[1]))
-                .pipe(gulp.dest('build/files/lib'));
+            var pipe = b.bundle()
+                .pipe(source(f[1]));
+            if (minify)
+                pipe.pipe(uglify());
+            pipe.pipe(gulp.dest('build/files/lib'));
         };
         b.plugin(tsify);
-        b.on('update', bundle);
+        if (update)
+            b.on('update', bundle);
         b.on('log', console.log);
         console.log(f[0]);
         bundle();
     });
 }
-gulp.add('js', combinejs);
+gulp.add('js', function () { return combinejs(false, true); });
 gulp.add('css', function () {
     gulp.src('lib/*.scss')
         .pipe(sass(sassOptions))
         .pipe(gulp.dest('build/files/lib'));
 });
-gulp.add("default", ['js', 'css']);
-gulp.add("watch", ['build'], function () {
-    gulp.watch('lib/*.scss', ['css']);
-});
-gulp.add("build", ['js', 'css'], function () {
+gulp.add('copyFiles', function () {
     gulp.src(['manifest.json', 'popup.html', 'images/*'], { base: '.' })
         .pipe(gulp.dest('build/files'));
 });
+gulp.add("default", ['js', 'css']);
+gulp.add("watch", ['copyFiles', 'css'], function () {
+    combinejs(true, false);
+    gulp.watch('lib/*.scss', ['css']);
+});
+gulp.add("build", ['copyFiles', 'js', 'css']);
 gulp.add('crx', ['build'], function () {
     gulp.src('build/files')
         .pipe(crx(crxOptions()))
--- gulpfile.ts
+++ gulpfile.ts
@@ -6,51 +6,57 @@ import * as browserify from "browserify"
 import * as tsify from "tsify"
 import * as source from "vinyl-source-stream"
 import * as watchify from "watchify"
+import * as uglify from "gulp-uglify"
 
 const sassOptions: sass.Options = {
-        outputStyle:'expanded'
-    },crxOptions = ():crx.options => {return {
-        privateKey:fs.readFileSync('build/ChromeEQDExt.pem','utf8'),
-        filename:'eqdExtra.crx'
-    }}
+    outputStyle: 'expanded'
+}, crxOptions = (): crx.options => {
+    return {
+        privateKey: fs.readFileSync('build/ChromeEQDExt.pem', 'utf8'),
+        filename: 'eqdExtra.crx'
+    }
+}
 
-function combinejs(){
+function combinejs(update: boolean, minify: boolean) {
 
-    [['lib/backScript.ts','backScript.js'],['lib/eqd-inject.ts','eqd-inject.js'],['lib/eqdComment-inject.ts','eqdComment-inject.js']].forEach(f=>{
-    let b= watchify(browserify({entries:f[0]})),
-        bundle = ()=>{
-            b.bundle()
-                .pipe(source(f[1]))
-                .pipe(gulp.dest('build/files/lib'))
-        }
+    [['lib/backScript.ts', 'backScript.js'], ['lib/eqd-inject.ts', 'eqd-inject.js'], ['lib/eqdComment-inject.ts', 'eqdComment-inject.js']].forEach(f => {
+        let b = watchify(browserify({ entries: f[0] })),
+            bundle = () => {
+                let pipe = b.bundle()
+                    .pipe(source(f[1]))
+                if (minify) pipe.pipe(uglify())
+                pipe.pipe(gulp.dest('build/files/lib'))
+            }
         b.plugin(tsify)
-        b.on('update',bundle)
-        b.on('log',console.log)
+        if (update) b.on('update', bundle)
+        b.on('log', console.log)
         console.log(f[0])
         bundle()
     })
 }
 
-gulp.add('js',combinejs)
+gulp.add('js', () => combinejs(false, true))
 
-gulp.add('css',()=>{
+gulp.add('css', () => {
     gulp.src('lib/*.scss')
         .pipe(sass(sassOptions))
         .pipe(gulp.dest('build/files/lib'))
 })
-
-gulp.add("default",['js','css'])
-
-gulp.add("watch",['build'],()=>{
-    gulp.watch('lib/*.scss',['css'])
-})
-
-gulp.add("build",['js','css'],()=>{
-    gulp.src(['manifest.json','popup.html','images/*'],{base:'.'})
+gulp.add('copyFiles', () => {
+    gulp.src(['manifest.json', 'popup.html', 'images/*'], { base: '.' })
         .pipe(gulp.dest('build/files'))
 })
 
-gulp.add('crx',['build'],()=>{
+gulp.add("default", ['js', 'css'])
+
+gulp.add("watch", ['copyFiles', 'css'], () => {
+    combinejs(true, false);
+    gulp.watch('lib/*.scss', ['css'])
+})
+
+gulp.add("build", ['copyFiles', 'js', 'css'])
+
+gulp.add('crx', ['build'], () => {
     gulp.src('build/files')
         .pipe(crx(crxOptions()))
         .pipe(gulp.dest('build'))
